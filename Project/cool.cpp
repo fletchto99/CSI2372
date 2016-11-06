d31607464bc44e3f6cc9e83e1c58ce23173f646b
Paulina Ramirez <paulinarmza@gmail.com>
2016-11-06 16:22:55 -0500

    i added cool file

---
+++ Project/cool.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Paulina on 2016-11-06.
+//
+
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,3 +1,10 @@
 /*
  * Main entry point
  */
+ using namespace std;
+#include <iostream>
+
+int main(){
+cout<<"hello world" <<endl;
+return 0;
+}
