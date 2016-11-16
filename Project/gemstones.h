3b024852ec90cd2569e18102459b7eb607e071c0
Matt Langlois <fletchto99@gmail.com>
2016-11-16 18:40:52 -0500

    Start implementing cards

--- Project/gemstones.h
+++ Project/gemstones.h
@@ -5,8 +5,60 @@
 #ifndef CSI2372_GEMSTONES_H
 #define CSI2372_GEMSTONES_H
 
+#include <string>
 
-class gemstones {
+using namespace std;
+
+class Card {
+    virtual int getCardsPerCoin(int coins) = 0;
+    virtual string getName() = 0;
+    virtual void print(const ostream& out) = 0;
+};
+
+class Quartz : public Card {
+
+    int getCardsPerCoin(int coins) {
+        switch(coins) {
+            case 1:
+                return 4;
+            case 2:
+                return 6;
+            case 3:
+                return 8;
+            case 4:
+                return 10;
+            default:
+                return -1; //TODO: return illegal argument error
+        }
+    }
+
+};
+
+class Hematite : public Card {
+
+};
+
+class Obsidian : public Card {
+
+};
+
+class Malachite : public Card {
+
+};
+
+class Turquoise : public Card {
+
+};
+
+class Ruby : public Card {
+
+};
+
+class Amethyst : public Card {
+
+};
+
+class Emerald : public Card {
 
 };
 
