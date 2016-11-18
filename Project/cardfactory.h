4f3a6c077eb1d8cee12e34da8dab24b111fcfba0
Paulina <paulina@Paulinas-MacBook-Pro.local>
2016-11-18 12:17:58 -0500

    no message

--- Project/cardfactory.h
+++ Project/cardfactory.h
@@ -1,9 +1,12 @@
 #ifndef CSI2372_CARDFACTORY_H
 #define CSI2372_CARDFACTORY_H
 
+#include "deck.h"
 
 class CardFactory {
-
+public:
+    static CardFactory* getFactory();
+    Deck getDeck();
 };
 
 
