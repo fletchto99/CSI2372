eb050f6888b01d4ee7f5fca1b09d61c4f69553dd
Paulina <paulina@Paulinas-MacBook-Pro.local>
2016-11-18 12:07:32 -0500

    defined TradeArea

--- Project/trade_area.h
+++ Project/trade_area.h
@@ -1,9 +1,22 @@
 #ifndef CSI2372_TRADEAREA_H
 #define CSI2372_TRADEAREA_H
 
+#include "gemstones.h"
+#include "cardfactory.h"
+#include <list>
 
+/**
+ * TradeArea holds the cards openly and support access insertion and removal.
+ */
 class TradeArea {
-
+private:
+    std::list<Card> d_list;
+public:
+    TradeArea& operator+=(Card*);
+    bool legal(Card*);
+    Card* trade(string);
+    int numCards();
+    TradeArea(std::istream&, CardFactory*);
 };
 
 
