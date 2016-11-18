8a03b5ef020b7523eaf3d363f848601c33340f1c
Paulina <paulina@Paulinas-MacBook-Pro.local>
2016-11-18 12:11:37 -0500

    defined Hand

--- Project/Hand.h
+++ Project/Hand.h
@@ -1,9 +1,16 @@
 #ifndef CSI2372_HAND_H
 #define CSI2372_HAND_H
 
+#include "gemstones.h"
+#include "cardfactory.h"
 
 class Hand {
-
+public:
+    Hand&operator+=(Card*);
+    Card* play();
+    Card* top();
+    Card* operator[](int);
+    Hand(std::istream&, CardFactory*);
 };
 
 
