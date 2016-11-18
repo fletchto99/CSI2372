980f0cbd12d135785713cd3d353c2ab40d34c74e
Matt Langlois <fletchto99@gmail.com>
2016-11-18 12:19:58 -0500

    Table skeleton

--- Project/Table.h
+++ Project/Table.h
@@ -1,8 +1,14 @@
 #ifndef CSI2372_TABLE_H
 #define CSI2372_TABLE_H
 
+#include <string>
 
 class Table {
+
+    Table(std::istream& file);
+
+    bool win( std::string& );
+    void print(std::ostream&);
 
 };
 
