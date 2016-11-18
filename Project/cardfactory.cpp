a86910dbe2c8b3e74b7775c0b5fe8cbfbf4c1b31
Matt Langlois <fletchto99@gmail.com>
2016-11-18 12:40:29 -0500

    Implement cardfactory

--- Project/cardfactory.cpp
+++ Project/cardfactory.cpp
@@ -1,1 +1,34 @@
 #include "cardfactory.h"
+
+CardFactory *CardFactory::getFactory() {
+    if (d_factory == nullptr) {
+        d_factory = new CardFactory();
+        CardFactory reference = (*d_factory);
+        for(size_t i = 0; i < 104; i++) {
+            if (i < 20) {
+                reference.d_deck.push_back(new Quartz());
+            } else if (i < 38) {
+                reference.d_deck.push_back(new Hematite());
+            } else if (i < 54) {
+                reference.d_deck.push_back(new Obsidian());
+            } else if (i < 68) {
+                reference.d_deck.push_back(new Malachite());
+            } else if (i < 80) {
+                reference.d_deck.push_back(new Turquoise());
+            } else if (i < 90) {
+                reference.d_deck.push_back(new Ruby());
+            } else if (i < 98) {
+                reference.d_deck.push_back(new Amethyst());
+            } else {
+                reference.d_deck.push_back(new Emerald());
+            }
+        }
+        //TODO: Create a cardfactory
+    }
+    return d_factory;
+}
+
+Deck CardFactory::getDeck() {
+    std::random_shuffle(d_deck.begin(), d_deck.end());
+    return d_deck;
+}
--- Project/cardfactory.h
+++ Project/cardfactory.h
@@ -1,9 +1,14 @@
 #ifndef CSI2372_CARDFACTORY_H
 #define CSI2372_CARDFACTORY_H
 
+#include <list>
+
 #include "deck.h"
 
 class CardFactory {
+private:
+    Deck<Card> d_deck;
+    static CardFactory* d_factory = nullptr;
 public:
     static CardFactory* getFactory();
     Deck getDeck();
