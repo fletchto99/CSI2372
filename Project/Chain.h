37d1676a8e8b60329b8ced14616af7d81f23721a
Paulina <paulina@Paulinas-MacBook-Pro.local>
2016-11-18 11:55:48 -0500

    implemented chain template

--- Project/Chain.h
+++ Project/Chain.h
@@ -5,4 +5,22 @@
 #ifndef CSI2372_CHAIN_H
 #define CSI2372_CHAIN_H
 
+#include <vector>
+#include "gemstones.h"
+#include "cardfactory.h"
+
+/**
+ * Initialized the template Chain
+ * d_cards vector
+ */
+template <class T>
+class Chain {
+private:
+    std::vector<T> d_cards;
+public:
+    Chain<T>& operator+=(Card*);
+    int sell();
+    Chain (std::istream&, CardFactory*);
+};
+
 #endif //CSI2372_CHAIN_H
