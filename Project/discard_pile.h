6b30b2e07004245a1814e655c4c333ceebb476f4
Matt Langlois <fletchto99@gmail.com>
2016-11-18 12:06:58 -0500

    Skeleton for the discard pile

--- Project/discard_pile.h
+++ Project/discard_pile.h
@@ -1,8 +1,24 @@
 #ifndef CSI2372_DISCARDPILE_H
 #define CSI2372_DISCARDPILE_H
 
+#include <vector>;
+
+#include "gemstones.h"
+#include "cardfactory.h"
+
+using namespace std;
 
 class DiscardPile {
+private:
+    std::vector<Card> d_cards;
+public:
+
+    DiscardPile( std::istream&, CardFactory* );
+
+    DiscardPile& operator+=(Card*);
+    Card* pickUp();
+    Card* top();
+    void print( std::ostream& );
 
 };
 
