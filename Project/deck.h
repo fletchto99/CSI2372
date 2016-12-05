cbad6456cd13a5de2d52f4ba769359559b71cb25
Matt Langlois <fletchto99@gmail.com>
2016-12-05 15:36:19 -0500

    Minor commenting changes

--- Project/deck.h
+++ Project/deck.h
@@ -10,22 +10,24 @@
  * The deck extends a vectory of type <T> which will eventually
  * store all of the cards, shuffled by the cardfactory
  */
-template<typename T>
+template <typename T>
 class Deck : public std::vector<T> {
 
 public:
     /**
      * Reconstructs the deck from file.
+     *
+     * @param out The input stream to rebuild the deck from
+     * @param cardFactory The cardfactory
      */
-    Deck(std::istream &, CardFactory *);
+    Deck(std::istream &input, CardFactory* cardFactory);
 
     /**
      * Returns and removes the top card from the deck
      *
      * @return The top card from the deck
      */
-    Card *draw();
+    Card* draw();
 
 };
-
 #endif
